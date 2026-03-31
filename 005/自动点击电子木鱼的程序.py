e6a37da70ec35d7d0a445174e4f508b942485247
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-03-31 22:13:14 +0800

    完成作业

--- 005/自动点击电子木鱼的程序.py
+++ 005/自动点击电子木鱼的程序.py
@@ -4,10 +4,12 @@ import time
 import pyperclip
 
 pyperclip.copy("https://www.lddgo.net/common/wooden-fish")
-pag.click(1689,1578,duration=1)
-pag.click(1521,90,duration=0.5)
+pag.hotkey("win",'s')
+time.sleep(0.2)
 pag.hotkey("ctrl",'v')
+time.sleep(0.2)
 pag.press('enter')
+time.sleep(0.2)
 for i in range(100):
     pag.doubleClick(1307,978)
 
